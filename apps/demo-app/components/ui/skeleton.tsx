2dfb7bc4e1947e1f8922706dbfe055e304fdc0c6
Bucky Maler <maler.bucky@gmail.com>
2026-01-02 16:32:46 -0500

    sync dependency versions

--- apps/demo-app/components/ui/skeleton.tsx
+++ apps/demo-app/components/ui/skeleton.tsx
@@ -2,8 +2,6 @@ import { cn } from '@/lib/utils';
 
 function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
-    // TODO: Fix type error. Introduced when setting up Turborepo. (low priority)
-    // @ts-expect-error: ?
     <div
       data-slot="skeleton"
       className={cn('bg-accent animate-pulse rounded-md', className)}
